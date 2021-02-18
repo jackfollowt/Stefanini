f2869bfe3bec6ae31dafa49bc0979dc788db64df
allan alem <allan.vogado@gmail.com>
2021-02-18 13:16:26 -0300

    Telas de front e comunicação rest

---
+++ app-site/src/app/_services/pessoa.service.ts
@@ -0,0 +1,21 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Pessoa } from '@/_models';
+
+@Injectable({ providedIn: 'root' })
+export class PessoaService {
+    constructor(private http: HttpClient) { }
+
+    getAll() {
+        return this.http.get<Pessoa[]>(`${config.apiUrl}/pessoa`);
+    }
+
+    register(pessoa: Pessoa) {
+        return this.http.post(`${config.apiUrl}/pessoa`, pessoa);
+    }
+
+    delete(id: number) {
+        return this.http.delete(`${config.apiUrl}/pessoas/${id}`);
+    }
+}
